c394db7cb752ccf434c16cf6cb80c35ed856fcac
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-04-16 02:59:12 +0300

    created component landing page and images folder

---
+++ src/app/landingpage/partone/partone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partone',
+  templateUrl: './partone.component.html',
+  styleUrls: ['./partone.component.css']
+})
+export class PartoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
