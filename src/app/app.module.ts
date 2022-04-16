96cbbbbf0ce9604a7884d7da5660fe13e7f2cdfe
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-04-17 02:35:54 +0300

    deleted component landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PartoneComponent } from './landingpage/partone/partone.component';
+
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PartoneComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
