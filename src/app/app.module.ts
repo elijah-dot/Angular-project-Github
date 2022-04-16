e2caebb2e699b654f71a89e63a82dfe7d0930dd1
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-04-16 09:21:54 +0300

    added a navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PartoneComponent } from './landingpage/partone/partone.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PartoneComponent
+    PartoneComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
